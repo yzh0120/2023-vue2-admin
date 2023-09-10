f1fe5fdb966e75ec212d55d816f5e2ca8344edb9
yzh0120 <644193385@qq.com>
2023-09-10 12:17:31 +0800

    up

--- src/layout/default/main/index.vue
+++ src/layout/default/main/index.vue
@@ -1,26 +1,40 @@
 <template>
-    <el-main class="default_2_el-main">
-      <el-scrollbar class="default_main_2_shu_el-scrollbar" ref="layoutScrollbarRef">
-        <div ref="keepAliveContainer" class="app_main" id="app_main">
-          <transition name="fade-transform" mode="out-in">
-            <keep-alive>
-              <router-view :key="key" />
-            </keep-alive>
-            <!-- <router-view v-else></router-view> -->
-          </transition>
-        </div>
-      </el-scrollbar>
-    </el-main>
+  <el-main class="default_2_el-main">
+    <el-scrollbar class="default_main_2_shu_el-scrollbar" ref="layoutScrollbarRef">
+      <div ref="keepAliveContainer" class="app_main" id="app_main">
+        <transition name="fade-transform" mode="out-in">
+          <keep-alive>
+            <router-view :key="key" />
+          </keep-alive>
+        </transition>
+      </div>
+    </el-scrollbar>
+  </el-main>
 </template>
 
 <script>
 export default {
-  data() { 
+  data() {
     return {}
+  },
+  mounted() {
+    if (this.$refs.keepAliveContainer) {
+      this.$root.keepAlive =
+        this.$refs.keepAliveContainer.childNodes[0].__vue__._vnode.componentInstance;
+    }
   },
   computed: {
     key() {
       return this.$route.path;
+    },
+  },
+  watch: {
+    // 监听路由的变化
+    $route: {
+      handler(to) {
+        this.$refs.layoutScrollbarRef.wrap.scrollTop = 0;
+      },
+      deep: true,
     },
   },
 }
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,6 @@
 const getters = {
-  // tagsViewList: state => state.tagsView.tagsViewList,
-  // currentTagView: state => state.tagsView.currentTagView,
+  tagsViewList: state => state.tagsView.tagsViewList,
+  currentTagView: state => state.tagsView.currentTagView,
   routes: state => state.router.routes,
   isCollapse: state => state.config.isCollapse,
   userInfo: state => state.user.userInfo,
